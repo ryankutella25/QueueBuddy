5b6774500cd4e1288ab780dab594551c4626d719
ryankutella25 <ryankutella25@yahoo.com>
2024-01-19 10:59:09 -0600

    Initial Commit

---
+++ QueueBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueueBuddy
+//
+//  Created by Ryan Kutella on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
