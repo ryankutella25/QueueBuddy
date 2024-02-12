f859780e757252581a1fcc142bd8294dcf85c232
ryankutella25 <ryankutella25@yahoo.com>
2024-02-12 13:02:33 -0600

    Got Spotify API to work

---
+++ QueueBuddy/Constants.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+//keys for spotify apk
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"queuebuddy://")!
+let spotifyClientId = "13d07531f1854cc09f09cc475d31db44"
+let spotifyClientSecretKey = "dac287c7f046433a8714fcf53829ca35"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
